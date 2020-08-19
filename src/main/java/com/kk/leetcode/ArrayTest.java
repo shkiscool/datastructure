e3980afae8696728d3cb5f940540bf08042fd2fa
suhuikun <572728626@qq.com>
2020-08-19 10:10:32 +0800

    feat: 整数反转

--- src/main/java/com/kk/leetcode/ArrayTest.java
+++ src/main/java/com/kk/leetcode/ArrayTest.java
@@ -387,4 +387,34 @@ public class ArrayTest {
         return result;
     }
 
+    /*    整数反转
+        注意:
+        假设我们的环境只能存储得下 32 位的有符号整数，
+        则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。*/
+    @Test
+    public void reverseTest() {
+        System.out.println(reverse(1234556));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        int i = Integer.MAX_VALUE+2;
+        System.out.println(i);
+    }
+
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
 }
